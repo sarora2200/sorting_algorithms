61e59ee6e10dad4eb3bd591e0d847d63d5e0775d
sallyMohamed <engsally@gmail.com>
2023-10-22 02:21:09 -0700

    heap sort

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -7,11 +7,10 @@
  */
 void swap_integers(int *I, int *J)
 {
-    int t;
-
-    t = *I;
-    *I = *J;
-    *J = t;
+int t;
+t = *I;
+*I = *J;
+*J = t;
 }
 
 /**
@@ -23,28 +22,24 @@ void swap_integers(int *I, int *J)
  */
 void heap_max(int *arr, size_t s, size_t ro, size_t b)
 {
-    size_t left, right, largest;
-
-    largest = ro;
-    left = 2 * ro + 1;
-    right = 2 * ro + 2;
-
-    if (left < b && arr[left] > arr[largest])
-        largest = left;
-
-    if (right < b && arr[right] > arr[largest])
-        largest = right;
-
-    if (largest != ro)
-    {
-        swap_integers(&arr[ro], &arr[largest]);
-        print_array(arr, s);
-        heap_max(arr, s, largest, b);
-    }
+size_t left, right, largest;
+largest = ro;
+left = 2 * ro + 1;
+right = 2 * ro + 2;
+if (left < b && arr[left] > arr[largest])
+largest = left;
+if (right < b && arr[right] > arr[largest])
+largest = right;
+if (largest != ro)
+{
+swap_integers(&arr[ro], &arr[largest]);
+print_array(arr, s);
+heap_max(arr, s, largest, b);
+}
 }
 
 /**
- * heap_sort - Sorts an array of integers in ascending order using the heap sort algorithm.
+ * heap_sort - SortArray in ascending order using the heap sort algorithm.
  * @array: An array of integers.
  * @size: The size of the array.
  *
@@ -52,18 +47,15 @@ void heap_max(int *arr, size_t s, size_t ro, size_t b)
  */
 void heap_sort(int *array, size_t size)
 {
-    int c;
-
-    if (array == NULL || size < 2)
-        return;
-
-    for (c = size / 2 - 1; c >= 0; c--)
-        heap_max(array, size, c, size);
-
-    for (c = size - 1; c > 0; c--)
-    {
-        swap_integers(&array[0], &array[c]);
-        print_array(array, size);
-        heap_max(array, size, 0, c);
-    }
+int c;
+if (array == NULL || size < 2)
+return;
+for (c = size / 2 - 1; c >= 0; c--)
+heap_max(array, size, c, size);
+for (c = size - 1; c > 0; c--)
+{
+swap_integers(&array[0], &array[c]);
+print_array(array, size);
+heap_max(array, size, 0, c);
+}
 }
